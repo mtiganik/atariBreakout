6c374cec0fac170f0f26a5d6b156f8599c5f5fc2
Mihkel <mtiganik@gmail.com>
2024-02-15 10:33:05 +0200

    ball bounces back from borders

--- ball.js
+++ ball.js
@@ -11,8 +11,11 @@ function initBall() {
   ball.style.height = "20px"
   ball.style.borderRadius = "10px"
   ball.style.bottom = "30px"
-  //ball.style.top = window.innerHeight - 100 + "px"
   ball.style.left = "50%"
+  let ballComputedStyle = window.getComputedStyle(ball)
+  ball.style.left = parseInt(ballComputedStyle.left) + "px"
+  ball.style.top = parseInt(ballComputedStyle.top) + "px"
+
 }
 
 document.addEventListener("keydown", event => {
@@ -20,12 +23,17 @@ document.addEventListener("keydown", event => {
     isPause = false
     const welcome = document.getElementById("welcome")
     welcome.style.display = "none"
-    let computedStyle = window.getComputedStyle(ball)
-    ball.style.left = parseInt(computedStyle.left) + "px"
-    ball.style.top = parseInt(computedStyle.top) + "px"
 
+    let [boardWidth,boardHeight, boardLeft, boardTop] = getBoardComputedStyles();
+    console.log(ball.offsetWidth)
     if(bInter == -1){
       bInter = setInterval(() => {
+        if(parseInt(ball.style.left) <= boardLeft || parseInt(ball.style.left) >= (boardWidth -ball.offsetWidth)){
+          dx = -dx
+        }
+        if(parseInt(ball.style.top) <= boardTop || parseInt(ball.style.top) >= (boardHeight- ball.offsetHeight)){
+          dy = -dy
+        }
         ball.style.left = parseInt(ball.style.left) + dx + "px"
         ball.style.top = parseInt(ball.style.top) + dy + "px"
       },ballHOP)
@@ -33,3 +41,14 @@ document.addEventListener("keydown", event => {
 
   }
 })
+
+function getBoardComputedStyles(){
+  let board = document.getElementById("board");
+  let boardComputedStyle = window.getComputedStyle(board)
+
+  let boardWidth = parseInt(boardComputedStyle.left) + parseInt(boardComputedStyle.width);
+  let boardHeight = parseInt(boardComputedStyle.top) + parseInt(boardComputedStyle.height);
+  let boardLeft = parseInt(boardComputedStyle.left)
+  let boardTop = parseInt(boardComputedStyle.top)
+  return [boardWidth,boardHeight, boardLeft, boardTop];
+}
